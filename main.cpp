5a38b488d24215032d7ac647693ea118c95f33e3
rotem1dvir <58340728+rotem1dvir@users.noreply.github.com>
2020-03-31 20:09:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include "OSM.h"
+
+
+int main(){
+
+    std::cout<<"sys call: "<<osm_syscall_time(1000000)<<std::endl;
+    std::cout<<"function call :"<<osm_function_time(1000000)<<std::endl;
+    std::cout<<"operation call: "<<osm_operation_time(1000000)<<std::endl;
+    return 0;
+
+
+
+
+
+}
+
